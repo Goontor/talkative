31a80dd7bf23993979380f0b496247c4cf3423de
jeremy garavet <jeremy.garavet@gmail.com>
2014-03-13 14:15:38 +0100

    page login,

---
+++ Talkative/src/com/example/talkative/Login.java
@@ -0,0 +1,15 @@
+package com.example.talkative;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
