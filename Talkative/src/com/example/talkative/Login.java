33c1151442871e927bc278c0b335f258270c5d6a
Goontor <claverie_pierre@hotmail.fr>
2014-05-26 15:42:23 +0200

    Creation of the chatManager

--- Talkative/src/com/example/talkative/Login.java
+++ Talkative/src/com/example/talkative/Login.java
@@ -2,14 +2,30 @@ package com.example.talkative;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class Login extends Activity {
-
-	@Override
+	TextView SignUp;
+	Intent SignUpInt;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		SignUpInt = new Intent(this, SignIn.class);
+		SignUp = (TextView) findViewById(R.id.signInLog);
+		SignUp.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				
+				startActivity(SignUpInt);
+				
+			}
+			
+		});
 	}
 
 }
