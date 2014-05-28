c630403d1aad098f1bfc45a5c81eec0612907229
Goontor <claverie_pierre@hotmail.fr>
2014-05-28 18:26:56 +0200

    Creating the slider

--- Talkative/gen/com/example/talkative/R.java
+++ Talkative/gen/com/example/talkative/R.java
@@ -9,9 +9,18 @@ package com.example.talkative;
 
 public final class R {
     public static final class array {
-        public static final int lang_arrays=0x7f060000;
+        public static final int lang_arrays=0x7f070000;
+        public static final int nav_drawer_items=0x7f070001;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int counter_text_bg=0x7f040004;
+        public static final int counter_text_color=0x7f040005;
+        public static final int list_background=0x7f040001;
+        public static final int list_background_pressed=0x7f040002;
+        public static final int list_divider=0x7f040003;
+        public static final int list_item_title=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,53 +29,77 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int anonyme=0x7f020000;
         public static final int anonyme2=0x7f020001;
         public static final int background=0x7f020002;
         public static final int background_connexion=0x7f020003;
-        public static final int ic_launcher=0x7f020004;
-        public static final int logo=0x7f020005;
+        public static final int counter_bg=0x7f020004;
+        public static final int ic_drawer=0x7f020005;
+        public static final int ic_drawer1=0x7f020006;
+        public static final int ic_drawer2=0x7f020007;
+        public static final int ic_home=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int list_item_bg_normal=0x7f02000a;
+        public static final int list_item_bg_pressed=0x7f02000b;
+        public static final int list_selector=0x7f02000c;
+        public static final int logo=0x7f02000d;
+        public static final int planetsearch=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000f;
-        public static final int editNom=0x7f090007;
-        public static final int editText1=0x7f090008;
-        public static final int emailsi=0x7f09000d;
-        public static final int imageView1=0x7f090006;
-        public static final int lostPasswordLog=0x7f090004;
-        public static final int matchingImage=0x7f090000;
-        public static final int passwordsi=0x7f09000c;
-        public static final int pseudosi=0x7f09000b;
-        public static final int signInLog=0x7f090005;
-        public static final int spinLang=0x7f09000a;
-        public static final int spinLangMatch=0x7f090002;
-        public static final int spinLangNat=0x7f090009;
-        public static final int spinLangNatMatch=0x7f090001;
-        public static final int submitsi=0x7f09000e;
-        public static final int textView1=0x7f090003;
+        public static final int action_settings=0x7f0a0019;
+        public static final int counter=0x7f0a0009;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int editNom=0x7f0a0011;
+        public static final int editText1=0x7f0a0012;
+        public static final int emailsi=0x7f0a0017;
+        public static final int frame_container=0x7f0a0001;
+        public static final int grettingCM=0x7f0a0003;
+        public static final int icon=0x7f0a0007;
+        public static final int imageView1=0x7f0a0010;
+        public static final int list_slidermenu=0x7f0a0002;
+        public static final int lostPasswordLog=0x7f0a000e;
+        public static final int matchingImage=0x7f0a0004;
+        public static final int passwordLog=0x7f0a000c;
+        public static final int passwordsi=0x7f0a0016;
+        public static final int pseudosi=0x7f0a0015;
+        public static final int signInLog=0x7f0a000f;
+        public static final int spinLang=0x7f0a0014;
+        public static final int spinLangMatch=0x7f0a0006;
+        public static final int spinLangNat=0x7f0a0013;
+        public static final int spinLangNatMatch=0x7f0a0005;
+        public static final int submitButtonLog=0x7f0a000d;
+        public static final int submitsi=0x7f0a0018;
+        public static final int textView1=0x7f0a000a;
+        public static final int title=0x7f0a0008;
+        public static final int usernamLog=0x7f0a000b;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int chat_matcher=0x7f030002;
+        public static final int activity_main=0x7f030000;
+        public static final int chat_matcher=0x7f030001;
+        public static final int drawer_list_item=0x7f030002;
         public static final int login=0x7f030003;
         public static final int profil=0x7f030004;
         public static final int signin=0x7f030005;
     }
     public static final class menu {
-        public static final int login=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int login=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int lang_prompt=0x7f050004;
-        public static final int title_activity_login=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int desc_list_item_icon=0x7f060009;
+        public static final int drawer_close=0x7f060006;
+        public static final int drawer_open=0x7f060005;
+        public static final int hello_world=0x7f060002;
+        public static final int lang_prompt=0x7f060004;
+        public static final int title_activity_login=0x7f060003;
+        public static final int title_item_detail=0x7f060008;
+        public static final int title_item_list=0x7f060007;
     }
     public static final class style {
         /** 
@@ -90,10 +123,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- Talkative/src/com/example/talkative/Login.java
+++ Talkative/src/com/example/talkative/Login.java
@@ -1,31 +1,123 @@
 package com.example.talkative;
 
+import org.jivesoftware.smack.XMPPException;
+
+import com.example.talkative.ConnexionService.LocalBinder;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends Activity {
 	TextView SignUp;
+	EditText Pseudo;
+	EditText Password;
 	Intent SignUpInt;
+	Intent LogInInt;
+	Button SubmitButton;
+	boolean mBounded;
+	ConnexionService mCService;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 		SignUpInt = new Intent(this, SignIn.class);
+		LogInInt = new Intent(this, ChatMatcher.class);
 		SignUp = (TextView) findViewById(R.id.signInLog);
-		SignUp.setOnClickListener(new OnClickListener(){
+		Pseudo = (EditText) findViewById(R.id.usernamLog);
+		Password = (EditText) findViewById(R.id.passwordLog);
+		SubmitButton = (Button) findViewById(R.id.submitButtonLog);
+		Intent i = new Intent(Login.this, ConnexionService.class);
+		Login.this.startService(i);
+		SubmitButton.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				ConnectMe task = new ConnectMe();
+			    task.execute(new String[] { "super string" });
+			}
+
+		});
+
+		SignUp.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View arg0) {
-				
+
 				startActivity(SignUpInt);
-				
+
 			}
-			
+
 		});
 	}
 
+	protected void onStart() {
+		Log.d("onstart", "onstart1");
+		super.onStart();
+		Log.d("onstart", "onstart1");
+		Intent mIntent = new Intent(this, ConnexionService.class);
+		Log.d("onstart", "onstart1");
+		bindService(mIntent, mConnection, Context.BIND_AUTO_CREATE);
+		Log.d("onstart", "onstart1");
+	};
+
+	ServiceConnection mConnection = new ServiceConnection() {
+
+		public void onServiceDisconnected(ComponentName name) {
+			Log.d("onstart", "onstartDic1");
+			Toast.makeText(Login.this, "Service is disconnected", 1000).show();
+			mBounded = false;
+			mCService = null;
+		}
+
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.d("onstart", "onstartCon1");
+			Toast.makeText(Login.this, "Service is connected", 1000).show();
+			mBounded = true;
+			LocalBinder mLocalBinder = (LocalBinder) service;
+			mCService = mLocalBinder.getConnexionInstance();
+		}
+	};
+	
+	
+	private class ConnectMe extends AsyncTask<String, Void, String> {
+		final ProgressDialog dialog = ProgressDialog.show(
+				Login.this, "Connecting...", "Please wait...",
+				false);
+		protected void onPreExecute() {
+			
+			dialog.show();
+		}
+
+		protected String doInBackground(String... arg0) {
+			try {
+				mCService.connect(Pseudo.getText().toString(),
+						Password.getText().toString());
+				Log.d("toto", "onclick2");
+			} catch (Exception e) {
+				Log.d("connexion error", " is " + e);
+			}
+			String response="malin";
+			return response;
+		}
+
+		protected void onPostExecute(String result) {
+			dialog.dismiss(); 
+			startActivity(LogInInt);
+		}
+
+	}
 }
